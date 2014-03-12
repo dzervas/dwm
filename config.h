cfae90dce5420ed90d016cacac1cefd1c0b91401
Dimitris Zervas <dzervas@dzervas.gr>
2014-03-12 23:51:09 +0200

    Added steam to rules

--- config.h
+++ config.h
@@ -23,8 +23,11 @@ static const Rule rules[] = {
 	 */
 	/* class		instance	title	tag	isfloating	monitor */
 	{ "Gimp",		NULL,		NULL,	0,	True,		-1 },
-	{ "Firefox",		NULL,		NULL,	1,	False,		1 },
+
+	{ "Steam",		NULL,		NULL,	2,	False,		0 },
 	{ "UXTerm",		NULL,		NULL,	1,	False,		0 },
+
+	{ "Firefox",		NULL,		NULL,	1,	False,		1 },
 	{ "Skype",		NULL,		NULL,	2,	False,		1 },
 };
 
